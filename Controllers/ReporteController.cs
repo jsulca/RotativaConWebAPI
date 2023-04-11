c3e983b9317aacbb7a2bb6aa8c483514e23b819b
Junior Sulca <juniorsulca@outlook.com>
2023-04-10 22:13:35 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ReporteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace RotativaConWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReporteController : ControllerBase
+    {
+        [HttpGet("factura")]
+        public async Task<IActionResult> Factura()
+        {
+            ViewAsPdf vista = new("~/Views/Reporte/Boleta.cshtml");
+            byte[] datos = await vista.BuildFile(ControllerContext);
+            
+            return new FileContentResult(datos, "application/pdf")
+            {
+                FileDownloadName = "Boleta.pdf"
+            };
+        }
+
+    }
+}
